e2dceae8203829fa8e88cec21d5cc6a4e1f11b00
ericcui <ericcui2005@gmail.com>
2024-12-23 11:57:08 -0500

    Basic endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+'use client';
 import Planner from './components/planner';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/solve');
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <div className="flex h-[100vh] w-[100vw] items-center justify-center">
