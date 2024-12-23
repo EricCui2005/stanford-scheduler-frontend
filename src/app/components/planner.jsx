30b8f4fff850800cec256deb1a2f2b9eb0866c36
ericcui <ericcui2005@gmail.com>
2024-12-23 00:21:18 -0500

    Schedule block gaps

--- src/app/components/planner.jsx
+++ src/app/components/planner.jsx
@@ -1,29 +1,30 @@
 import QuarterBlock from './quarterBlock';
+import PlannerColumn from './plannerColumn';
 
 export default function Planner() {
   return (
     <>
-      <div className="ml-[5vw] flex h-[90vh] w-[90vw]">
-        <div className="h-full w-1/4">
+      <div className="ml-[5vw] flex h-[90vh] w-[90vw] gap-1">
+        <PlannerColumn>
           <QuarterBlock title="FRESH FALL" />
           <QuarterBlock title="FRESH WINTER" />
           <QuarterBlock title="FRESH SPRING" />
-        </div>
-        <div className="åh-full w-1/4">
+        </PlannerColumn>
+        <PlannerColumn>
           <QuarterBlock title="SOPH FALL" />
           <QuarterBlock title="SOPH WINTER" />
           <QuarterBlock title="SOPH SPRING" />
-        </div>
-        <div className="h-full w-1/4">
+        </PlannerColumn>
+        <PlannerColumn>
           <QuarterBlock title="JUNIOR FALL" />
           <QuarterBlock title="JUNIOR WINTER" />
           <QuarterBlock title="JUNIOR SPRING" />
-        </div>
-        <div className="h-full w-1/4">
+        </PlannerColumn>
+        <PlannerColumn>
           <QuarterBlock title="SENIOR FALL" />
           <QuarterBlock title="SENIOR WINTER" />
           <QuarterBlock title="SENIOR SPRING" />
-        </div>
+        </PlannerColumn>
       </div>
     </>
   );
---
+++ src/app/components/plannerColumn.jsx
@@ -0,0 +1,7 @@
+export default function PlannerColumn({ children }) {
+  return (
+    <>
+      <div className="flex h-full w-1/4 flex-col gap-1">{children}</div>
+    </>
+  );
+}
