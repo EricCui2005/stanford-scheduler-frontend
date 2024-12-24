0ba14edd30e37ea5ebcc8ef52013f6b2300d44f3
ericcui <ericcui2005@gmail.com>
2024-12-23 19:03:29 -0500

    Added course titles and truncating text

--- src/app/components/classBlock.jsx
+++ src/app/components/classBlock.jsx
@@ -1,10 +1,8 @@
-export default function ClassBlock({ code, title }) {
+export default function ClassBlock({ completeTitle }) {
   return (
     <>
-      <div className="flex w-[95%] items-center rounded-md border border-black bg-gray-600 px-1 text-sm text-white">
-        <p>
-          {code}: {title}
-        </p>
+      <div className="flex w-[95%] items-center rounded-md border border-black bg-gray-600 px-2 text-sm text-white">
+        <p className="overflow-hidden whitespace-nowrap">{completeTitle}</p>
       </div>
     </>
   );
--- src/app/components/planner.jsx
+++ src/app/components/planner.jsx
@@ -32,80 +32,80 @@ export default function Planner() {
         <PlannerColumn>
           <QuarterBlock title="FRESH FALL">
             {schedule &&
-              schedule['FRESH']['FRESH_FALL'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['FRESH']['FRESH_FALL'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="FRESH WINTER">
             {schedule &&
-              schedule['FRESH']['FRESH_WINTER'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['FRESH']['FRESH_WINTER'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="FRESH SPRING">
             {schedule &&
-              schedule['FRESH']['FRESH_SPRING'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['FRESH']['FRESH_SPRING'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
         </PlannerColumn>
         <PlannerColumn>
           <QuarterBlock title="SOPH FALL">
             {schedule &&
-              schedule['SOPH']['SOPH_FALL'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SOPH']['SOPH_FALL'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="SOPH WINTER">
             {schedule &&
-              schedule['SOPH']['SOPH_WINTER'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SOPH']['SOPH_WINTER'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="SOPH SPRING">
             {schedule &&
-              schedule['SOPH']['SOPH_SPRING'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SOPH']['SOPH_SPRING'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
         </PlannerColumn>
         <PlannerColumn>
           <QuarterBlock title="JUNIOR FALL">
             {schedule &&
-              schedule['JUNIOR']['JUNIOR_FALL'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['JUNIOR']['JUNIOR_FALL'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="JUNIOR WINTER">
             {schedule &&
-              schedule['JUNIOR']['JUNIOR_WINTER'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['JUNIOR']['JUNIOR_WINTER'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="JUNIOR SPRING">
             {schedule &&
-              schedule['JUNIOR']['JUNIOR_SPRING'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['JUNIOR']['JUNIOR_SPRING'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
         </PlannerColumn>
         <PlannerColumn>
           <QuarterBlock title="SENIOR FALL">
             {schedule &&
-              schedule['SENIOR']['SENIOR_FALL'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SENIOR']['SENIOR_FALL'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="SENIOR WINTER">
             {schedule &&
-              schedule['SENIOR']['SENIOR_WINTER'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SENIOR']['SENIOR_WINTER'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
           <QuarterBlock title="SENIOR SPRING">
             {schedule &&
-              schedule['SENIOR']['SENIOR_SPRING'].map((course) => (
-                <ClassBlock key={course} code={course} title={course} />
+              schedule['SENIOR']['SENIOR_SPRING'].map((completeTitle) => (
+                <ClassBlock key={completeTitle} completeTitle={completeTitle} />
               ))}
           </QuarterBlock>
         </PlannerColumn>
